badb4e4e2f66b25f2daf57d3ab6e44978738f3cb
SurajPhulara <suraj66171@gmail.com>
2023-07-19 07:27:09 +0530

    completed home page hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div className='home'>
       <div className="hero_section">
-        <div className="flex flex-col justify-between mt-16 h-64 text-2xl w-5/12">
-          <div className="intro w-fit"> Hello, my name is <span className="name"> Suraj Phulara </span></div>
-          <div className="profession text-7xl font-bold w-fit">I'm a
+        <div className="flex flex-col justify-between mt-16 h-fit gap-8 text-2xl w-2/5">
+          <div className="intro w-max"> Hello, my name is <span className="name text-2xl w-fit"> Suraj Phulara </span></div>
+          <div className="profession text-7xl font-bold w-max">I'm a
             <Typewriter
               options={{
                 strings: [`Developer`, 'Designer'],
@@ -18,8 +18,12 @@ const Home = () => {
               }}
             />
           </div>
-          <div className="description text-lg">
-          I'm a Full-Stack Web Developer. My expertise is to create and design Websites,Apps,Mockups and many more...
+          <div className="description text-lg w-11/12">
+          I'm a Full-Stack Web Developer and UX/UI Designer who loves bringing digital ideas to life. I specialize in creating stunning websites and eye-catching mockups that captivate and engage users.
+          </div>
+          <div className="flex gap-8">
+          <a href="#" className="btn yellowbg text-xl hover:shadow-[0_4px_0px_rgb(0,0,0,.2)]"> Download CV </a>
+          <a href="#" className="btn text-xl bg-white text-blue-600 font-semibold hover:shadow-[0_4px_0px_rgb(0,0,0,.2)]"> Hire Me Now </a>
           </div>
         </div>
         <div className="image_container">
