37b7f9ce561c20aa976a9cd39a168afa17b3a124
SurajPhulara <suraj66171@gmail.com>
2023-07-20 14:10:49 +0530

    updated responsive bits of code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,10 +3,10 @@ import "./Cards.css"
 
 const Cards = (props) => {
   return (
-    <div className="card max-w-sm rounded-xl overflow-hidden shadow-2xl w-64 h-[19rem] bg-white flex flex-col justify-center items-center gap-4 ">
+    <div className="card max-w-sm rounded-xl overflow-hidden shadow-2xl w-64 h-[19rem] bg-white flex flex-col justify-center items-center gap-4 pl-4 pr-4 ">
       <img src={props.image} className=" w-20" alt="" />
       <p className=" text-blue-600 font-semibold text-xl">{props.title}</p>
-      <p className="text-gray-500 text-center text-base">{props.text}</p>
+      <p className="text-gray-500 text-center text-lg">{props.text}</p>
     </div>
   )
 }
