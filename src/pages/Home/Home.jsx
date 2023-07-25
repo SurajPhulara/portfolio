d8846e942ea454f874b63d17264252461368a8a0
SurajPhulara <suraj66171@gmail.com>
2023-07-25 15:33:44 +0530

    removed darkmode styling from remaining elements

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -122,7 +122,7 @@ const Home = () => {
                 <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="+91 1234567890" />
               </div>
               <div class="">
-                <label for="email" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Email address</label>
+                <label for="email" class="block mb-2 text-lg font-medium text-gray-900">Email address</label>
                 <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="IamBatman@gmail.com" required />
               </div>
             </div>
