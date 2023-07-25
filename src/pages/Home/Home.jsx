7e54d3e07895a14c2efc66a98111080ae99ab6e6
SurajPhulara <suraj66171@gmail.com>
2023-07-25 15:32:06 +0530

    removed darkmode styling from form elements

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -121,7 +121,7 @@ const Home = () => {
                 <label for="phone" class="block mb-2 text-lg font-medium text-gray-900">Phone number</label>
                 <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="+91 1234567890" />
               </div>
-              <div class="mb-6">
+              <div class="">
                 <label for="email" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Email address</label>
                 <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="IamBatman@gmail.com" required />
               </div>
@@ -129,11 +129,11 @@ const Home = () => {
 
 
             <div class="mb-6">
-              <label for="message" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Message</label>
-              <textarea id="message" rows="3" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Message" required />
+              <label for="message" class="block mb-2 text-lg font-medium text-gray-900">Message</label>
+              <textarea id="message" rows="3" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Message" required />
             </div>
 
-            <div className=" text-center">
+            <div className=" text-center w-full">
               <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-xl text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </div>
           </form>
