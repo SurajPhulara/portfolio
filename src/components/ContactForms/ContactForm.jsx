833a536663d076ad2d804e39d98004a74ec22456
SurajPhulara <https://github.com/SurajPhulara>
2023-09-29 09:07:04 +0530

    changed placeholders

--- src/components/ContactForms/ContactForm.jsx
+++ src/components/ContactForms/ContactForm.jsx
@@ -21,11 +21,11 @@ const ContactForm = () => {
             <div class="grid gap-6 mb-6 md:grid-cols-2 text-2xl">
               <div>
                 <label for="first_name" class="block mb-2 text-lg font-medium text-gray-900">First name</label>
-                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Bruce" required />
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="First name" required />
               </div>
               <div>
                 <label for="last_name" class="block mb-2 text-lg font-medium text-gray-900">Last name</label>
-                <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Wayne" />
+                <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Last name" />
               </div>
               <div>
                 <label for="phone" class="block mb-2 text-lg font-medium text-gray-900">Phone number</label>
@@ -33,7 +33,7 @@ const ContactForm = () => {
               </div>
               <div class="">
                 <label for="email" class="block mb-2 text-lg font-medium text-gray-900">Email address</label>
-                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="IamBatman@gmail.com" required />
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="abc@gmail.com" required />
               </div>
             </div>
 
